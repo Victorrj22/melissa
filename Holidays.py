ae145071bfb9b4a7f43c16117ac369ba05c0ad5d
Danilo Maia Florenzano <daniloflorenzano1@outlook.com>
2025-01-10 22:39:21 -0300

    adiciona tipagem onde possivel e ajusta formatacao do codigo

--- Holidays.py
+++ Holidays.py
@@ -1,10 +1,11 @@
-﻿import requests;
+﻿import requests
 from typing import List, Optional
 
-token = "17321|H0sCrqpfrb5QNeRXA5mQDxD243eyaqIP";
+token: str = "17321|H0sCrqpfrb5QNeRXA5mQDxD243eyaqIP"
+
 
 class Holiday:
-    #Faz o mapemanto do retorno da API
+    #Faz o mapeamento do retorno da API
     def __init__(self, date: str, name: str, holiday_type: str, level: str, law: Optional[str] = None):
         self.date = date
         self.name = name
@@ -17,8 +18,8 @@ class Holiday:
         return (f"Holiday(Date: {self.date}, Name: {self.name}, Type: {self.holiday_type}, "
                 f"Level: {self.level}, Law: {self.law})")
 
-def get_holidays(state: str, year: int, token: str) -> List[Holiday]:
-    endpoint = f"https://api.invertexto.com/v1/holidays/{year}?token={token}&state={state}";
+def get_holidays(state: str, year: int, api_token: str) -> List[Holiday]:
+    endpoint = f"https://api.invertexto.com/v1/holidays/{year}?token={api_token}&state={state}"
     try:
         response = requests.get(endpoint, timeout=10)
         response.raise_for_status()
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-﻿from langchain_ollama import ChatOllama
-import Holidays as holidays
-import Generic as generic
+﻿from typing import Callable
+from langchain_ollama import ChatOllama
+import Holidays as Holidays
+from Holidays import Holiday
+import Generic as Generic
 from ollama import ChatResponse
 
-# question = "Estamos no mês de Janeiro de 2025, quais são os feriados desse nês em SP?";
 question = "[data atual: 10/01/2025] Quais feriados nesse mes em Sao Paulo?"
-model=ChatOllama(model="llama3.2", format="json")
+model = ChatOllama(model="llama3.2", format="json")
 
-#Aqui é feito o 'treinamento' da IA de acordo com cada função
-model = model.bind_tools(
-    tools = [
+# Aqui é feito o 'treinamento' da IA de acordo com cada função
+model = model.bind_tools(  # type: ignore
+    tools=[
         {
             "name": "get_holidays",
             "description": "Get the holidays of the year for a specific state",
@@ -20,12 +21,12 @@ model = model.bind_tools(
                         "type": "string",
                         "description": "The state abbreviation to get holidays for ('SP', 'RJ', 'ES')."
                     },
-                    "year":{
+                    "year": {
                         "type": "integer",
                         "description": "The year to get holidays for"
                     }
                 },
-                "required": ["state","year"]
+                "required": ["state", "year"]
             }
         },
         {
@@ -46,43 +47,68 @@ model = model.bind_tools(
     ],
 )
 
-functions = {
-    "get_holidays": holidays.get_holidays,
-    "generic_response": generic.generic_response
+functions: dict[str, Callable[[str, int, str], list[Holiday]] | Callable[[str], ChatResponse]] = {
+    "get_holidays": Holidays.get_holidays,
+    "generic_response": Generic.generic_response
 }
 
-def invoke_and_run(model, invoke_arg):
-    result = model.invoke(invoke_arg)
-    if result:
-        tool_calls = result.tool_calls
-        #Dentro de tool_calls ficam os mapeamentos que a IA realizou de acordo com o input e o metodo reconhecido
-        if tool_calls:
-            for tool_call in tool_calls:
-                function_name = tool_call.get('name')
-                #Esses são os parâmetros para a função reconhecida
-                arguments = tool_call.get('args')
-                if arguments and function_name:
-                    function = functions.get(function_name)
-                    #Por enquanto temos apenas 1 verificação para get_holidays. Quando houver outras funções, colocaremos em outros 'else'
-                    if function and function_name == 'get_holidays':
-                        state = arguments.get('state')
-                        year = arguments.get('year')
-                        if state and year:
-                            #Chama a função passando os parâmetros reconhecidos
-                            holidays_list = function(state=state, year=year, token=holidays.token)
-                            re_input = "Dada essa pergunta: '" + question + "', os feriados são: " + str(holidays_list) + ". Responda a pergunta de forma resumida e direta e como se estivesse conversando oralmente."
-                            response: ChatResponse = generic.generic_response(re_input)
-                            print(response.message.content)
-                        else:
-                            print(f"Argumentos incompletos: {arguments}")
-                    #Caso o modelo identifique que é uma pergunta genérica, entra no fluxo do modelo Ollama
-                    elif function and function_name == 'generic_response':
-                        response = generic.generic_response(question=question)
-                        message = response.get('message')
-                        content = message.get('content')
-                        print('Pergunta:' + question)
-                        print ('Resposta:' + content)
-                else:
-                    print(f"Função ou argumentos ausentes em: {tool_call}")
+def invoke_and_run(llm_model, prompt) -> str:
+    try:
+        generic_error_message = "Não foi possível encontrar uma resposta para a pergunta"
+        result = llm_model.invoke(prompt)
+        if not result:
+            return generic_error_message
 
-invoke_and_run(model, question)
+        tool_calls = result.tool_calls
+        if not tool_calls:
+            return generic_error_message
+
+        # Dentro de tool_calls ficam os mapeamentos que a IA realizou conforme o input e o método reconhecido
+        for tool_call in tool_calls:
+            function_name: str = tool_call.get('name')
+            function_parameters: dict[str, str] = tool_call.get('args')
+
+            if not function_parameters or not function_name:
+                return f"Função ou argumentos ausentes em: {tool_call}"
+
+            function: Callable[[str, int, str], list[Holiday]] | Callable[[str], ChatResponse] | None = (
+                functions.get(function_name))
+            chat_response: ChatResponse
+
+            if function is Holidays.get_holidays:
+                state: str | None = function_parameters.get('state')
+                year_as_str: str | None = function_parameters.get('year')
+                year: int | None = int(year_as_str) if year_as_str else None
+                if state and year:
+                    holidays_list: list[Holiday] = function(state=state, year=year, token=Holidays.token)  # type: ignore
+
+                    re_input = ("Dada essa pergunta: '" + question + "', os feriados são: "
+                                + str(holidays_list)
+                                + ". Responda a pergunta de forma resumida e direta e como se estivesse conversando oralmente."
+                                )
+
+                    chat_response = Generic.generic_response(re_input)
+                    if isinstance(chat_response.message.content, str):
+                        return chat_response.message.content
+                    else:
+                        return generic_error_message
+
+                else:
+                    print(f"Argumentos incompletos: {function_parameters}")
+
+            # Caso o modelo identifique que é uma pergunta genérica, entra no fluxo do modelo Ollama
+            elif function is Generic.generic_response:
+                assert function is Generic.generic_response
+                chat_response = function(question=question)  # type: ignore
+                if isinstance(chat_response.message.content, str):
+                    return chat_response.message.content
+                else:
+                    return generic_error_message
+
+        return generic_error_message
+
+    except Exception as e:
+        return f"Erro ao executar a função: {e}"
+
+
+print(invoke_and_run(model, question))
